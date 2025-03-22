14e9e25a142dc7f53050a6976e3c24972bb969b3
delowerhossentuhin <dhtuhinmcc@gmail.com>
2025-03-23 01:51:12 +0600

    Updated Sort Function

--- 6_Sort_NumpyArray.py
+++ 6_Sort_NumpyArray.py
@@ -5,3 +5,18 @@ sys.stdout=open('output.txt','w')
 
 nparray=np.array([[3,1,5,7,8],[8,4,1,0,8]])
 print(np.sort(nparray))
+
+#Alphabetical
+list=["john","arya","Sansa","Bran","Theon"]
+nparray2=np.array(list)
+print(nparray2)
+print(np.sort(nparray2)) # Block letter to small letter
+
+#boolean
+nparray3=np.array([True,False,True,False])
+print(nparray3)
+print(np.sort(nparray3))
+#2d array
+nparray4=np.array([[0,1,8,2,4,6,3,9,0],[4,1,3,2,8,3,6,1,9]])
+print(nparray4)
+print(np.sort(nparray4)) #Sort inside each individual row
